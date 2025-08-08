0debfe724c7c3daaaf085fc3c1baa15b8c47b639
omiras <oscar.miras@gmail.com>
2025-08-08 12:05:10 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
-import IdCard from './components/IdCard.vue';
-
 
 </script>
 
 <template>
   <h1>Creación de componentes con Vue</h1>
-  <section>
-    <h2>Id Card</h2>
-    <IdCard last-name='Doe' first-name='John' gender='male' :height="178" :birth='new Date("1992-07-14")'
-      picture="https://randomuser.me/api/portraits/men/44.jpg" />
-  </section>
+  <p>Crea a conitnuación todos los componentes del ejercicio.</p>
 </template>
 
 <style scoped>
-h2 {
-  color: blueviolet;
-}
+
 </style>
